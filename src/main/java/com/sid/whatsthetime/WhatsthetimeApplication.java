ff6f3dd915a15c018cc3ec8fd7205441bdd5296b
aeehrtw <26565327+aeehrtw@users.noreply.github.com>
2024-03-27 11:15:15 +0530

    Create the project

---
+++ src/main/java/com/sid/whatsthetime/WhatsthetimeApplication.java
@@ -0,0 +1,13 @@
+package com.sid.whatsthetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsthetimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsthetimeApplication.class, args);
+	}
+
+}
