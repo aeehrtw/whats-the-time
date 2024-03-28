2a3151610ab97d6410b30337d64132bbaa5649c5
aeehrtw <26565327+aeehrtw@users.noreply.github.com>
2024-03-28 15:13:39 +0530

    Add the final application, command line runner code. Update README with instructions to execute.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # whats-the-time
-What's the time
+What's the time project
+
+# How to run
+
+1. Download the source code
+2. Execute the spring boot application 
+   - (I used my IntelliJ IDE.)
+   - You can use `.\mvnw spring-boot:run` to execute
+   - Change the `timeString` to test it out
+3. Alternatively check the WhatsthetimeApplication.java, uncomment the lines as mentioned in the comment. Execute to get a commandline util which takes inputs in a loop.
--- src/main/java/com/sid/whatsthetime/WhatsthetimeApplication.java
+++ src/main/java/com/sid/whatsthetime/WhatsthetimeApplication.java
@@ -1,13 +1,60 @@
 package com.sid.whatsthetime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class WhatsthetimeApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(WhatsthetimeApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(WhatsthetimeApplication.class, args);
+
+		SpringApplication.run(WhatsthetimeApplication.class, args).close();
 	}
+
+	@Bean
+	CommandLineRunner runner() {
+
+
+		/////// Change the time here to test
+		String timeString = "10:10";
+
+		Time time = new Time(timeString);
+		TimeTeller timeTeller = new TimeTeller();
+		System.out.println(timeTeller.tellTheTime(time));
+
+
+
+
+		//////// Alternatively,,, Uncomment the below lines to run as a command line utility
+
+//		while (true) {
+//			System.out.println("Enter the time string, or Q or q to Quit");
+//			Scanner scan = new Scanner(System.in);
+//			String input = scan.nextLine();
+//
+//			if ( input.contains("Q") || input.contains("q") ) {
+//				break;
+//			} else {
+//				try {
+//					Time time = new Time(input.trim());
+//					TimeTeller timeTeller = new TimeTeller();
+//					System.out.println(timeTeller.tellTheTime(time));
+//				} catch (IllegalArgumentException e) {
+//					System.out.println("Invalid input. Try again. " +
+//							"\nExpected formats are hh:mm or h:mm with valid hours(hh/h) and minute(mm) values\n");
+//				}
+//			}
+//		}
+
+        return args -> {};
+    }
 
 }
